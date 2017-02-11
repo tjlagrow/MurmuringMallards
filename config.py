455b869f27c131de821d441228d538b6240e3c88
Theodore J. LaGrow <tjlagrow@gmail.com>
2017-02-11 15:18:49 -0800

    there

---
+++ config.py
@@ -0,0 +1,5 @@
+consumer_key = 'CdvdaweEMdP1mzvBRX5mc653X' #'YOUR-CONSUMER-KEY'
+consumer_secret = 'HEhzmHoqqpzhRbTPFllQs5DJucRxd0a88rb6DhTRuaI4sr5avH' #'YOUR-CONSUMER-SECRET'
+access_token = '830545747129016322-DvMTQrwXTsZHeZXTzAseB5tXYzWSLOb' #'YOUR-ACCESS-TOKEN'
+access_secret = 'NyD3rQDgkSlvrFzSOd1EfBIe20904LTCdYE1eKS6vWuH7' #'YOUR-ACCESS-SECRET'
+ 
